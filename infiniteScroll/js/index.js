7e8f6ae20a3527dba17625e3d5e27350facccb6c
Rahul <rahul25796@gmail.com>
2019-09-15 00:01:05 +0530

    updated coded

--- infiniteScroll/js/index.js
+++ infiniteScroll/js/index.js
@@ -1,15 +1,25 @@
 const footer = document.querySelector("footer");
 const section = document.querySelector("section");
 
+const RandomImgUrls = [
+  "https://source.unsplash.com/user/erondu/800x600",
+  "https://source.unsplash.com/user/erondu/800",
+  "https://source.unsplash.com/random/800x600",
+  "http://lorempixel.com/800/600",
+  "https://placeimg.com/800/600/any",
+  "https://picsum.photos/800/600",
+  "https://picsum.photos/800"
+];
+
 const callback = function(entries, observer) {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
-      console.log("yeyyy");
-
       var img = document.createElement("img");
-      img.setAttribute("src", "https://source.unsplash.com/random/");
+      img.setAttribute(
+        "src",
+        RandomImgUrls[Math.floor(Math.random() * RandomImgUrls.length)]
+      );
       img.setAttribute("alt", "Random");
-
       section.appendChild(img);
     }
   });
